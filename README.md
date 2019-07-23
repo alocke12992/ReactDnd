c32db4ee9282b1552aa3989004f54f9bbe5d67af
Andrew Locke <alocke12992@gmail.com>
2019-07-23 00:57:59 -0700

    Set up for npm

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # React DND
 A simple drag and drop package with zero dependencies, implemented using react hooks
+
+## Install
+`npm install react-dnd --save`
+or
+`yarn add react-dnd`
+
+## Requirements
+
+Must have react 16.8+ installed
+
+## Usage
+
+Import ReactDnd, then pass an array to the ReactDnd component using the `items` param
+
+```jsx
+import React from 'react';
+import ReactDnd from 'react-dnd';
+
+const myNumbers = ["1", "2", "3"]
+
+function App() {
+  return (
+    <div>
+      <ReactDnd
+        items={myNumbers}
+      />
+    </div>
+  )
+}
+```
--- src/App.js
+++ src/App.js
@@ -1,51 +1,57 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-
-const App = () => {
+import React, { useState, useEffect } from 'react';
+const ReactDnd = ({ items }) => {
+  console.log(items)
   const [selectedItem, setSelectedItem] = useState(null)
-  const [items, setItems] = useState([
-    "😀 Smile",
-    "🙁 Frown",
-    "😇 Angel",
-    "😈 Devil",
-  ])
+  const [itemList, setItemList] = useState([])
+
+  useEffect(() => {
+    setItemList(items);
+  }, [])
 
   const handleDragStart = (e, i) => {
-    setSelectedItem(items[i])
+    setSelectedItem(itemList[i])
     e.dataTransfer.effectAllowed = "move";
     e.dataTransfer.setData("text/html", e.target);
     e.dataTransfer.setDragImage(e.target, 20, 20);
   }
 
   const handleDragOver = (i) => {
-    const draggedOver = items[i];
+    const draggedOver = itemList[i];
 
     if (draggedOver === selectedItem) return;
 
-    let newItems = items.filter(item => item !== selectedItem);
+    let newItems = itemList.filter(item => item !== selectedItem);
     newItems.splice(i, 0, selectedItem);
 
-    setItems(newItems);
+    setItemList(newItems);
   }
-
-  return (
-    <div className="App">
-      { items.map( (item, i) => (
-        <div
-          key={i}
-          className="item"
-          draggable
-          onDragStart={(e) => handleDragStart(e, i)}
-          onDragOver={() => handleDragOver(i)}
-          onDragEnd={() => setSelectedItem(null)}
-        >
-          <Menu />
-          <span>{item}</span>
+  if (itemList.length) {
+    return itemList.map((item, i) => (
+      <div
+        key={i}
+        className="item-container"
+        draggable
+        onDragStart={(e) => handleDragStart(e, i)}
+        onDragOver={() => handleDragOver(i)}
+        onDragEnd={() => setSelectedItem(null)}
+      >
+        <div className="item">
+          {item}
         </div>
-      ))}
-    </div>
-  );
+      </div>
+    ))
+  }
+  return null
 };
+
+const items = [
+  "😀 Smile",
+  "🙁 Frown",
+  "😇 Angel",
+  "😈 Devil",
+]
+
+const App = () => <ReactDnd items={items} />
 
 
 export default App;
